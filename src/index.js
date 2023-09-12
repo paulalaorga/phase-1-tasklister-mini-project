265bc53b5bf9a26e3bf1c38bc8d401b350e60d0a
paula <paulalaorga@gmail.com>
2023-09-12 13:03:51 +1000

    index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+
+  function handleSubmit(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    // Access the input element and its value
+    const input = document.querySelector("#new-task-description");
+    const inputValue = input.value;
+
+    // Do something with the input value (for example, display it)
+    alert("You entered: " + inputValue);
+
+    // You can also submit the form using JavaScript if needed
+    // form.submit();
+  }
+
+  // Add the event listener to the form
+  form.addEventListener("submit", handleSubmit);
 });
